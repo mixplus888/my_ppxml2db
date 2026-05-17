b76543a79829b3f6efc381353eca036f28852ee6
vtc <tab10.acer@gmail.com>
2026-05-17 14:02:46 +0800

    Build: Ensure Dockerfile uses local copies of ALL engine files

---
+++ xact.sql
@@ -0,0 +1,22 @@
+CREATE TABLE xact(
+_id INTEGER NOT NULL PRIMARY KEY,
+uuid VARCHAR(36) NOT NULL UNIQUE,
+type VARCHAR(20) NOT NULL,
+acctype VARCHAR(10) NOT NULL, -- requires to instantiate AccountTransaction vs PortfolioTransaction, but redundant otherwise
+account VARCHAR(36) NOT NULL REFERENCES account(uuid),
+date VARCHAR(32) NOT NULL,
+currency VARCHAR(16) NOT NULL,
+amount BIGINT NOT NULL,
+security VARCHAR(36) REFERENCES security(uuid),
+shares BIGINT NOT NULL,
+fees BIGINT NOT NULL DEFAULT 0,
+taxes BIGINT NOT NULL DEFAULT 0,
+note TEXT,
+source VARCHAR(255),
+updatedAt VARCHAR(64) NOT NULL,
+_xmlid INT NOT NULL,
+_order INT NOT NULL
+);
+
+CREATE UNIQUE INDEX xact__uuid ON xact(uuid);
+CREATE INDEX xact__account ON xact(account);
